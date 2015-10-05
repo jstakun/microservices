4f0e079417b152504b22589298f708b7ad172d25
jstakun <jstakun@redhat.com>
2015-10-05 11:28:44 +0200

    fixed params

--- eventbus/src/main/java/com/redhat/refarch/microservices/eventbus/EventBusInboundService.java
+++ eventbus/src/main/java/com/redhat/refarch/microservices/eventbus/EventBusInboundService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -17,34 +18,30 @@ public class EventBusInboundService {
 	
 	@POST
 	@Path("/event/customer/{id}")
-	@Consumes({"application/json", "application/xml"})
 	@Produces({"application/xml"})
-	public Response newCustomerEvent(Long id) {
+	public Response newCustomerEvent(@PathParam("id") Long id) {
 		logInfo("New customer " + id + " created");
 		return Response.status(200).entity("<status>newCustomer " + id + " event received</status>").build();
 	}
 	
 	@POST
 	@Path("/event/order/{id}")
-	@Consumes({"application/json", "application/xml"})
 	@Produces({"application/xml"})
-	public Response newOrderEvent(Long id) {
+	public Response newOrderEvent(@PathParam("id") Long id) {
 		logInfo("New order " + id + " created");
 		return Response.status(200).entity("<status>newOrder " + id + " event received</status>").build();
 	}
 	
 	@POST
 	@Path("/event/product/{id}")
-	@Consumes({"application/json", "application/xml"})
 	@Produces({"application/xml"})
-	public Response newProductEvent(Long id) {
+	public Response newProductEvent(@PathParam("id") Long id) {
 		logInfo("New product " + id + " created");
 		return Response.status(200).entity("<status>newProduct " + id + " event received</status>").build();
 	}
 	
 	@GET
 	@Path("/info")
-	@Consumes({"*/*"})
 	@Produces({"application/xml"})
 	public Response info() {
 		return Response.status(200).entity("<info><name>Event bus service</name><version>1.0.1</version></info>").build();

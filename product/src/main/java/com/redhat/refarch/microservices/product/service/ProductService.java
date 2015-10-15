4c563be7af69824feabf98fc9fe3178fcf19ab47
jstakun <jstakun@redhat.com>
2015-10-15 10:09:01 +0200

    new version

--- product/src/main/java/com/redhat/refarch/microservices/product/service/ProductService.java
+++ product/src/main/java/com/redhat/refarch/microservices/product/service/ProductService.java
@@ -52,7 +52,7 @@ public class ProductService
 
 	private Logger logger = Logger.getLogger( getClass().getName() );
 
-	private static final String VERSION = "1.0.2";
+	private static final String VERSION = "1.0.1";
 	
 	@Path("/products")
 	@POST

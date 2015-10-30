f76f8dce82f62f5334f244faee2ddc31122e365c
jstakun <jstakun@redhat.com>
2015-10-30 14:14:15 +0100

    added status check

--- product/src/main/java/com/redhat/refarch/microservices/product/service/ProductService.java
+++ product/src/main/java/com/redhat/refarch/microservices/product/service/ProductService.java
@@ -295,6 +295,25 @@ public class ProductService
 		return Response.status(200).entity("<info><name>Product service</name><version>" + VERSION + "</version><ip>" + addr + "</ip></info>").build();
 	}
 
+	@GET
+	@Path("/_status/dbhealthz")
+	@Produces({"application/xml"})
+	public Response dbhealthz() {
+		int count = 0;
+		try {
+			count = em.createNamedQuery( "Product.findFeatured", Product.class ).getResultList().size();
+			logger.log(Level.INFO, "Checked database status with response: " + count);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, e.getMessage(), e);
+		}
+		
+		if (count > 0) {
+			return Response.status(200).entity("<dbhealtz>ok</dbhealtz>").build();
+		} else {
+			return Response.status(500).entity("<dbhealtz>failed</dbhealtz>").build();
+		}
+	}
+	
 	@Target({ElementType.METHOD})
 	@Retention(RetentionPolicy.RUNTIME)
 	@HttpMethod("PATCH")

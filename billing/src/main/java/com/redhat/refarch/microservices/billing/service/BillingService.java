6e6b7ed8e3b8c4fd1aa0302b25c333669d791277
jstakun <jstakun@redhat.com>
2016-10-04 15:57:15 +0200

    new billing version

--- billing/src/main/java/com/redhat/refarch/microservices/billing/service/BillingService.java
+++ billing/src/main/java/com/redhat/refarch/microservices/billing/service/BillingService.java
@@ -27,7 +27,7 @@ public class BillingService
 
 	private static final Random random = new Random();
 	
-	private static final String VERSION = "1.0.3b34";
+	private static final String VERSION = "1.0.3b35";
 
 	@POST
 	@Path("/process")
